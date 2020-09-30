c1ce1642bd097cbae44616d88fe7f8699c8e099c
Sil van Diepen <me@sil.mt>
2020-09-30 11:58:43 +0200

    export functions

--- dist/index.js
+++ dist/index.js
@@ -11,4 +11,5 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 __exportStar(require("./types"), exports);
+__exportStar(require("./functions"), exports);
 //# sourceMappingURL=index.js.map
